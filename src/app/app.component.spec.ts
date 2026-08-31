7ca2461acb2febd40bc0401d2b3bfd59e645dc8c
Robert Rios <rtrios006@gmail.com>
2026-08-31 17:24:34 -0400

    Add baseline Angular 18 unit tests for individual components.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'about-me-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('about-me-app');
+  it(`should render site navigation`, () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.querySelector('app-site-nav')).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, about-me-app');
+  it(`should render site content`, () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.querySelector('app-about')).toBeTruthy();
+    expect(element.querySelector('app-growing')).toBeTruthy();
+    expect(element.querySelector('app-hero')).toBeTruthy();
+    expect(element.querySelector('app-journal')).toBeTruthy();
   });
+
+  it(`should render site footer`, () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.querySelector('app-site-footer')).toBeTruthy();
+  });
+
 });
